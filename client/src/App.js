9f39c5deb9bee0e6886f8efd79d2a4d513e67498
SantiRC <santiagoramirescardona@gmail.com>
2023-08-30 17:10:35 -0500

    Avance

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import LandingPage from "./components/LandingPage/LandingPage";
+import Form from './components/Form/Form';
 import { BrowserRouter, Routes , Route } from "react-router-dom";
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <Routes>
     
       <Route exact path="/" element={<LandingPage/>}></Route>
-   
+      <Route path="/Form" element={<Form/>}></Route>
     </Routes>
   </BrowserRouter>
   );
