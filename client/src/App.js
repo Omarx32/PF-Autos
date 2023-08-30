318681daa50aab080213408b58207218b4ab0a6b
MIGorriti <miguegorriti@gmail.com>
2023-08-30 18:52:24 -0300

    second update home

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
-
-import './App.css';
+import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
-import { BrowserRouter, Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+
+import Form from "./components/Form/Form";
+
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    
-      <Route exact path="/" element={<LandingPage/>}></Route>
-   
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}></Route>
+        <Route exact path="/Home" element={<Home />}></Route>
+        <Route exact path="/Form" element={<Form />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
