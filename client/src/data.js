a75b476d4a59b80f4c6b4936856dda51338cd0b2
Mateoch14 <89880893+Mateoch14@users.noreply.github.com>
2023-08-31 09:54:50 -0300

    primer commit de rama card

---
+++ client/src/data.js
@@ -0,0 +1,21 @@
+const cars = [
+    {
+        "id": 782585,
+        "title": "for mustang",
+        "image": "https://www.ford.com.ar/content/dam/Ford/website-assets/latam/ar/nameplate/mustang/mustang-2021/colorizer/gris-magnetico/far-mustang-fighter.jpg.dam.full.high.jpg/1622062693051.jpg",
+        "imageType": "jpg"
+    },
+    {
+        "id": 716426,
+        "title": "ecosport",
+        "image": "https://imgd.aeplcdn.com/664x374/cw/ec/40369/Ford-EcoSport-Right-Front-Three-Quarter-159249.jpg?wm=0&q=80&q=80",
+        "imageType": "jpg"
+    },
+    {
+        "id": 715497,
+        "title": "crossovers",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqLxQkRPnny4gL3D5fXfCvFck8m9PG2rve6Q&usqp=CAU",
+        "imageType": "jpg"
+    } ]
+
+    export default cars
