3e1c647eb87c21173b79c37b1a88631af2eda756
MiGorriti <miguegorriti@gmail.com>
2023-08-31 16:42:12 -0300

    imagenes carrousel

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Navbar from "../NavBar/Navbar";
 import styles from "../Home/Home.module.css";
 import Footer from "../Footer/Footer";
 import Cards from "../Cards/Cards";
@@ -8,22 +8,23 @@ export default function Home() {
   return (
     <div>
       <div className={styles.background}>
-        <h1 className={styles.AppTitle}>NN</h1>
-        <h2 className={styles.SubTitle}>Todas Las Categorias</h2>
-        <div classname={styles.enlaces}>
-          <a href="/" target="_blank">
-            <p>Prueba</p>
-          </a>
-          <a href="/Form" target="_blank">
-            Form
-          </a>
+        <h1 className={styles.AppTitle}>IGNITE MOTORS</h1>
+        <Navbar></Navbar>
+      </div>
+      <h2 className={styles.SubTitle}>Todas Las Categorias</h2>
+      <div classname={styles.enlaces}>
+        <a href="/" target="_blank">
+          <p>Prueba</p>
+        </a>
+        <a href="/Form" target="_blank">
+          Form
+        </a>
+      </div>
+      <div className="App">
+        <div>
+          <Cards cars={cars}></Cards>
         </div>
-        <div className="App">
-          <div>
-            <Cards cars={cars}></Cards>
-          </div>
-          <Footer />
-        </div>
+        <Footer />
       </div>
     </div>
   );
