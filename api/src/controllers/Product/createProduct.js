1d71370405a47025befcd567b4193386e0835c8b
Omarx32 <rmarin.0812@gmail.com>
2023-09-13 01:21:38 -0430

    listo el form

--- api/src/controllers/Product/createProduct.js
+++ api/src/controllers/Product/createProduct.js
@@ -1,101 +1,73 @@
-const { Category, Product, Brand} = require("../../db")
+const { Category, Product, Brand } = require("../../db");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({ 
+  cloud_name: 'dfg0dgm9r', 
+  api_key: '589471853742194', 
+  api_secret: 'uU8OMiDxQdFYnJibegcyC0kzIqw' 
+});
 
 async function createProduct(req, res) {
-    try {
-        const productsInput = Array.isArray(req.body) ? req.body : [req.body];
-        const products = [];
 
-        for (const productInput of productsInput) {
-            const {
-                name,
-                brand,
-                description,
-                price,
-                stock,
-                maker,
-                model,
-                color,
-                kilometraje,
-                direccion,
-                category,
-                image
-            } = productInput;
+    const productsInput = Array.isArray(req.body) ? req.body : [req.body];
 
-            if (
-                !name ||
-                !brand ||
-                !description ||
-                !price ||
-                !stock ||
-                !maker ||
-                !model ||
-                !color ||
-                !kilometraje ||
-                !direccion
-                || !image
-            ) {
-                throw new Error("Missing required data");
-            }
+    for (const productInput of productsInput) {
+      const {name, brand, description, price, stock, maker, model, color, kilometraje, direccion, category, image,} = productInput;
 
-            const newProduct = {
-                name,
-                brand,
-                description,
-                price,
-                stock,
-                maker,
-                model,
-                color,
-                kilometraje,
-                direccion,
-                image,
-            };
+      if (!name ||!brand ||!description ||!price ||!stock ||!maker ||!model ||!color ||!kilometraje || !direccion ||!image) { 
+         throw new Error("Missing required data");}
+    
+        const imageUrls = [];
+        for (const imageData of productInput.image){
+            const result = await cloudinary.uploader.upload(imageData,{
+                folder: 'productsDetail'
+            })
+            console.log(result);
+            imageUrls.push(result.secure_url)
+        }   
 
-            const createdProduct = await Product.create(newProduct); 
+      const newProduct = {
+        name,
+        category,
+        brand,
+        description,
+        price,
+        stock,
+        maker,
+        model,
+        color,
+        kilometraje,
+        direccion,
+        image:imageUrls,
+      };
+     
 
-            if(brand){
-                const brandInstance = await Brand.findOne({ where: { name: brand } });
-                if(brandInstance){
-                    await newProduct.setBrand(brandInstance);
-                }
-            }
-            // Manejar las relaciones de categoría
-            const categories = category;
-
-            if (categories && categories.length > 0) {
-                const categoriesExistentes = await Category.findAll({
-                    where: {
-                        name: categories,
-                    },
-                });
-
-                const categoriessNoExistentes = categories.filter(
-                    (gen) =>
-                        !categoriesExistentes.map((g) => g.name).includes(gen)
-                );
-
-                const newCategories = await Category.bulkCreate(
-                    categoriessNoExistentes.map((name) => ({ name })),
-                    { returning: true }
-                );
-
-                await createdProduct.setCategories([
-                    ...categoriesExistentes,
-                    ...newCategories,
-                ]);
-            }
-
-            products.push(createdProduct);
+      const createdProduct = await Product.create(newProduct);
+      const marcaNombre = newProduct.brand; // Aquí asumo que marcaNombre es el nombre de la marca
+      if (marcaNombre) {
+        const marca = await Brand.findOne({ where: { name: marcaNombre } }); // Busca la instancia de la marca por nombre
+        if (!marca) {
+          throw new Error(`Marca "${marcaNombre}" no encontrada`);
         }
-
-        res.status(201).json(products);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(400).json({ mensaje: "Error al crear nuevos productos", error });
+        await createdProduct.setBrand(marca); // Establecer la relación de marca
+      }
+      const categorys = newProduct.category;
+      if (categorys) {
+        const category = await Category.findOne({ where: { name: categorys } });
+        if (!categorys) {
+          throw new Error(`Categoría "${category}" no encontrada`);
+        }
+        await createdProduct.setCategory(category);
+      }
+      
+     
+      // Manejar las relaciones de categoría
+      return createdProduct
     }
+   
+
 }
 
-
-module.exports= {
-    createProduct,
-}
+module.exports = {
+  createProduct,
+};
